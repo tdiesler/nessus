7c9a83e50f710d40f6e3c99166815ee67c90e240
Thomas Diesler <tdiesler@redhat.com>
2018-07-15 19:48:51 +0200

    [resolves #21] Update project license

--- bitcoin/src/test/java/io/nessus/test/bitcoin/WalletTest.java
+++ bitcoin/src/test/java/io/nessus/test/bitcoin/WalletTest.java
@@ -1,5 +1,25 @@
 package io.nessus.test.bitcoin;
 
+/*-
+ * #%L
+ * Nessus :: Bitcoin
+ * %%
+ * Copyright (C) 2018 Nessus
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import static io.nessus.Wallet.ALL_FUNDS;
 import static io.nessus.Wallet.LABEL_DEFAULT;
 
@@ -18,9 +38,6 @@ import io.nessus.bitcoin.AbstractBitcoinTest;
 
 public class WalletTest extends AbstractBitcoinTest {
 
-    /**
-     * Send 10 BTC to Bob from the default account 
-     */
     @Test
     public void testSimpleSpending () throws Exception {
 
--- cipher/src/main/java/io/nessus/cipher/ECIESCipher.java
+++ cipher/src/main/java/io/nessus/cipher/ECIESCipher.java
@@ -1,5 +1,25 @@
 package io.nessus.cipher;
 
+/*-
+ * #%L
+ * Nessus :: Cipher
+ * %%
+ * Copyright (C) 2018 Nessus
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.security.GeneralSecurityException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -15,17 +35,6 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import io.nessus.cipher.utils.DeterministicRandom;
 
-/*
- * InvalidKeyException: Illegal key size
- * http://www.bouncycastle.org/wiki/display/JA1/Frequently+Asked+Questions
- * 
- * $ find /Library/Java/JavaVirtualMachines/jdk1.8.0_171.jdk -name *_policy.jar
- * 
- * Make sure these are on the classpath
- * 
- * .../jre/lib/security/policy/unlimited/local_policy.jar
- * .../jre/lib/security/policy/unlimited/US_export_policy.jar
- */
 public class ECIESCipher {
 
     static {
