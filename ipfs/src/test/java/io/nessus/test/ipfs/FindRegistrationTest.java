70f5b20704ee3ef033e8d2932564912273f490cb
Thomas Diesler <tdiesler@redhat.com>
2018-11-12 11:13:52 +0100

    [resolves #51] Verify that address registration survives wallet restart

--- ipfs/src/test/java/io/nessus/test/ipfs/FindRegistrationTest.java
+++ ipfs/src/test/java/io/nessus/test/ipfs/FindRegistrationTest.java
@@ -12,6 +12,7 @@ public class FindRegistrationTest extends AbstractWorkflowTest {
     @Override
     boolean unlockUnspentAfterTest() {
         // set to false when simulating wallet restart
+        // rm -rf ~/.bitcoin/regtest
         return true;
     }
     
@@ -29,3 +30,20 @@ public class FindRegistrationTest extends AbstractWorkflowTest {
         Assert.assertNotNull(pubKey);
     }
 }
+
+/*
+
+- First Run
+
+PubKey register: [addr=n3ha6rJa8ZS7B4v4vwNWn8CnLHfUYXW1XE, ro=false, labels=[Bob]] => Tx fc9cf7c6406239fc45964859430c13776c14cbec34c2f305aae15b1959f28687
+Lock unspent: fc9cf7c6406239fc45964859430c13776c14cbec34c2f305aae15b1959f28687 1
+PubKey Tx: fc9cf7c6406239fc45964859430c13776c14cbec34c2f305aae15b1959f28687 => [addr=n3ha6rJa8ZS7B4v4vwNWn8CnLHfUYXW1XE, ro=false, labels=[Bob]] => MDYwEAYHKoZIzj0CAQYFK4EEABwDIgAEmrmigcXiUFTpId22iIFfHeq+hl6zJ47S5bIWxVpLpGs=
+
+- Close and reopen the wallet 
+- Verify that registration utxo in unlocked (due to wallet restart)
+- Rerun the test  
+
+PubKey Tx: fc9cf7c6406239fc45964859430c13776c14cbec34c2f305aae15b1959f28687 => [addr=n3ha6rJa8ZS7B4v4vwNWn8CnLHfUYXW1XE, ro=false, labels=[Bob]] => MDYwEAYHKoZIzj0CAQYFK4EEABwDIgAEmrmigcXiUFTpId22iIFfHeq+hl6zJ47S5bIWxVpLpGs=
+Lock unspent: fc9cf7c6406239fc45964859430c13776c14cbec34c2f305aae15b1959f28687 1
+
+*/
